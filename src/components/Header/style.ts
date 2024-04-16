a986dc3c67819138fe5fe62db187fd6d3d2f3ee6
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-04-16 10:57:10 -0300

    Fix header styles

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,12 +1,21 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-  padding: 0 10rem;
+  width: 70rem;
   height: 6.5rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin: 0 auto;
+
+  @media only screen and (max-width: 1120px) {
+    width: 48rem;
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 26.5rem;
+  }
+
 `
 
 export const HeaderOptions = styled.div`
